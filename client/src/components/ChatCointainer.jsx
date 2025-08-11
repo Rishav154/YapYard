345a7a07d9611933be5dfb3281f4b676e30cb99b
Rishav154 <rishav15045@gmail.com>
2025-08-11 18:58:50 +0530

    chat container update

--- client/src/components/ChatCointainer.jsx
+++ client/src/components/ChatCointainer.jsx
@@ -83,17 +83,27 @@ const ChatCointainer = () => {
       </div>
 
         {/* ----------Bottom-------------*/}
-        <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
-          <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-            <input onChange={(e)=> setInput(e.target.value)} value={input}  onKeyDown= {(e)=> e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
-            <input onChange={handleSendImage} type="file"  id="image" accept='image/png, image/jpeg' hidden/>
-            <label htmlFor="image">
-              <img src= {assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' /> 
-              </label>
-          </div>
-          <img onClick={handleSendMessage} src={assets.send_button} alt="" className='w-7 cursor-pointer' />
-
-        </div>
+<form 
+  onSubmit={handleSendMessage} 
+  className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'
+>
+  <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+    <input 
+      onChange={(e) => setInput(e.target.value)} 
+      value={input}
+      type="text" 
+      placeholder='Send a message' 
+      className='flex-1 text-sm p-3 border-none rounded-lg outline-none bg-transparent text-white placeholder-gray-400'
+    />
+    <input onChange={handleSendImage} type="file" id="image" accept='image/png, image/jpeg' hidden />
+    <label htmlFor="image">
+      <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+    </label>
+  </div>
+  <button type="submit">
+    <img src={assets.send_button} alt="Send" className='w-7 cursor-pointer' />
+  </button>
+</form>
 
     </div>
   ): (
